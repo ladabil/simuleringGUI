236489409d1b36b49ec1b9c6685b9045e80941cc
spriti <spriti@mail.com>
2013-10-19 19:25:18 +0200

    Feilfix

--- public_html/pdf/index.php
+++ public_html/pdf/index.php
@@ -58,9 +58,9 @@ $lightDiff = $row['lightDiff'];
 $numHvit = $row['numHvit'];
 $numBrun = $row['numBrun'];
 $climateZone = $row['climateZone'];
-if($climateZone == '1'){ $climateZone = "Sør-Norge, kyst";}
-if($climateZone == '2'){ $climateZone = "Sør-Norge, innland";}
-if($climateZone == '3'){ $climateZone = "Sør-Norge, høyfjell";}
+if($climateZone == '1'){ $climateZone = "SÃ¸r-Norge, kyst";}
+if($climateZone == '2'){ $climateZone = "SÃ¸r-Norge, innland";}
+if($climateZone == '3'){ $climateZone = "SÃ¸r-Norge, hÃ¸yfjell";}
 if($climateZone == '4'){ $climateZone = "Midt-Norge, kyst";}
 if($climateZone == '5'){ $climateZone = "Midt-Norge, innland";}
 if($climateZone == '6'){ $climateZone = "Nord-Norge, kyst";}
@@ -73,7 +73,7 @@ if($building == '1') {$building = "Enebolig";}
 if($building == '2') {$building = "Leilighet";}
 if($building == '3') {$building = "Rekkehus";}
 		
-if($houseBuildYear == '1') {$houseBuildYear = "Før 1987";}
+if($houseBuildYear == '1') {$houseBuildYear = "FÃ¸r 1987";}
 if($houseBuildYear == '2') {$houseBuildYear = "Mellom 1987 og 1997";}
 if($houseBuildYear == '3') {$houseBuildYear = "Etter 1997";}
 
@@ -97,15 +97,15 @@ $pdf->Cell(0,6, 'Hus', 0,2,'l');
 $pdf->SetFont('Times','',12);
 $pdf->ln(2);
 $pdf->Cell(0,5, utf8_decode('Bygg Type: '.$building) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Byggeår: '.$houseBuildYear) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('ByggeÃ¥r: '.$houseBuildYear) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('Brutto Areal: '.$houseTotalArea) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('P-Rom: '.$housePrimaryArea) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Byggeår: '.$houseBuildYear) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('ByggeÃ¥r: '.$houseBuildYear) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('Veggareal ytre: '.$ytterveggAreal) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('Takareal: '.$yttertakAreal) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Vindu og Dør areal: '.$vinduDorAreal) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('Vindu og DÃ¸r areal: '.$vinduDorAreal) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('Indre luftvolum: '.$luftVolum) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Ønsket innetemp: '.$onsketTemp) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('Ã˜nsket innetemp: '.$onsketTemp) ,0,2,'l');
 
 // undertittel for Oppvarming
 $pdf->ln(5);
@@ -115,13 +115,13 @@ $pdf->Cell(0,6, 'Oppvarming', 0,2,'l');
 // verdier
 $pdf->SetFont('Times','',12);
 $pdf->ln(2);
-$pdf->Cell(0,5, utf8_decode('Primær Oppvarming: '.$priHeat) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Sekundær Oppvarming: '.$secHeat) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('PrimÃ¦r Oppvarming: '.$priHeat) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('SekundÃ¦r Oppvarming: '.$secHeat) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('Varme Differanse: '.$heatDiff.'%') ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Gulvvarme Vannbåren: '.$floorHeatWa) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('Gulvvarme VannbÃ¥ren: '.$floorHeatWa) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('Gulvvarme Elektrisk: '.$floorHeatEl) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Primær Elektrokjel (liter): '.$priBoilerSize) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Primær Elektrokjel (watt): '.$priBoilerPower) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('PrimÃ¦r Elektrokjel (liter): '.$priBoilerSize) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('PrimÃ¦r Elektrokjel (watt): '.$priBoilerPower) ,0,2,'l');
 
 
 // undertittel for Lys
@@ -133,8 +133,8 @@ $pdf->Cell(0,6, 'Lys', 0,2,'l');
 $pdf->SetFont('Times','',12);
 $pdf->ln(2);
 $pdf->Cell(0,5, utf8_decode('Antall Lyskilder: '.$numLight) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Primær Belysning: '.$priLightType) ,0,2,'l');
-$pdf->Cell(0,5, utf8_decode('Sekundær Belysning: '.$secLightType) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('PrimÃ¦r Belysning: '.$priLightType) ,0,2,'l');
+$pdf->Cell(0,5, utf8_decode('SekundÃ¦r Belysning: '.$secLightType) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('Gjennomsnittlig Brennetid (timer per dag): '.$lightTime) ,0,2,'l');
 $pdf->Cell(0,5, utf8_decode('Lys Differanse: '.$lightDiff.'%') ,0,2,'l');
 
