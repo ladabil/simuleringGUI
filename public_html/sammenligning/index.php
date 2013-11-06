bd2ba67161ea99effb122202c2cb1ffd98700b36
Mikael Bendiksen <duuxman@gmail.com>
2013-11-06 23:55:15 +0100

    dropdown meny test

--- public_html/sammenligning/index.php
+++ public_html/sammenligning/index.php
@@ -1,206 +1,129 @@
-<?php
-/*
-function jj_readcsv($filename, $header=false) {
-$handle = fopen($filename, "r");
-echo '<table>';
-//display header row if true
-if ($header) {
-    $csvcontents = fgetcsv($handle);
-    echo '<tr>';
-    foreach ($csvcontents as $headercolumn) {
-        echo "<th>$headercolumn</th>";
-    }
-    echo '</tr>';
-}
-// displaying contents
-while ($csvcontents = fgetcsv($handle)) {
-    echo '<tr>';
-    foreach ($csvcontents as $column) {
-        echo "<td>$column</td>";
-    }
-    echo '</tr>';
-}
-echo '</table>';
-fclose($handle);
-}
-
-jj_readcsv('http://jenna.bendiksens.net/~gruppe2/resultater/20131104_1383603361.xml.csv',true);
+<form name=	"testform" id="testform" action="?x=submit" method="post">
+  <select name="dropdown" id="dropdown">
+  	<option value="nothing" selected="selected">Velg Simuleringsresultat</option>
+  	<?php
+  	$mysqli = new mysqli("jenna.bendiksens.net", "gruppe2it", "123", "gruppe2it");
 
 
-
-
-	$row = 1; 
-	if (($handle = fopen("http://jenna.bendiksens.net/~gruppe2/resultater/20131104_1383603361.xml.csv", "r")) !== FALSE) 
+	$sql = "SELECT * FROM SimTask";
+		
+		if ( ($res = $mysqli->query($sql)) === FALSE )
 		{
-			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
-			{
-				 $num = count($data); 
-				 echo "<br />","Record #",$row,"<br />"; 
-				 $row++; 
-				 for ($recordcount=0; $recordcount < $num; $recordcount++) 
-				 {
-				 	 echo $data[$recordcount] . "<br />\n"; 
-				 } 
-			} 
-			fclose($handle); 
-		} 
+			die("error");
+		}
+
+		while($row = mysqli_fetch_array($res))
+		{
+			echo "<option value='http://jenna.bendiksens.net/~gruppe2/resultater/".$row['xmlId'].".xml.csv'>".$row['xmlId']."</option>";
+		}
+  	
+  	?>
+  </select>
+  <select name="dropdown2" id="dropdown">
+  	<option value="nothing" selected="selected">Velg Simuleringsresultat</option>
+  	<?php
+  	$mysqli = new mysqli("jenna.bendiksens.net", "gruppe2it", "123", "gruppe2it");
 
 
+	$sql = "SELECT * FROM SimTask";
+		
+		if ( ($res = $mysqli->query($sql)) === FALSE )
+		{
+			die("error");
+		}
 
+		while($row = mysqli_fetch_array($res))
+		{
+			echo "<option value='http://jenna.bendiksens.net/~gruppe2/resultater/".$row['xmlId'].".xml.csv'>".$row['xmlId']."</option>";
+		}
+  	
+  	?>
+  </select>
+  <input type="submit" name="Submit" value="Submit" id="Submit">
+</form>
 
-// returns a two-dimensional array or rows and fields
-$file = fopen('http://jenna.bendiksens.net/~gruppe2/resultater/20131104_1383603361.xml.csv', 'r');
-while (($line = fgetcsv($file)) !== FALSE) {
-  //$line is an array of the csv elements
-  print_r($line);
-}
-fclose($file);
-
-
-function csvstring_to_array($string, $separatorChar = ',', $enclosureChar = '"', $newlineChar = "\n") {
-    // @author: Klemen Nagode
-    $array = array();
-    $size = strlen($string);
-    $columnIndex = 0;
-    $rowIndex = 0;
-    $fieldValue="";
-    $isEnclosured = false;
-    for($i=0; $i<$size;$i++) {
-
-        $char = $string{$i};
-        $addChar = "";
-
-        if($isEnclosured) {
-            if($char==$enclosureChar) {
-
-                if($i+1<$size && $string{$i+1}==$enclosureChar){
-                    // escaped char
-                    $addChar=$char;
-                    $i++; // dont check next char
-                }else{
-                    $isEnclosured = false;
-                }
-            }else {
-                $addChar=$char;
-            }
-        }else {
-            if($char==$enclosureChar) {
-                $isEnclosured = true;
-            }else {
-
-                if($char==$separatorChar) {
-
-                    $array[$rowIndex][$columnIndex] = $fieldValue;
-                    $fieldValue="";
-
-                    $columnIndex++;
-                }elseif($char==$newlineChar) {
-                    echo $char;
-                    $array[$rowIndex][$columnIndex] = $fieldValue;
-                    $fieldValue="";
-                    $columnIndex=0;
-                    $rowIndex++;
-                }else {
-                    $addChar=$char;
-                }
-            }
-        }
-        if($addChar!=""){
-            $fieldValue.=$addChar;
-
-        }
-    }
-
-    if($fieldValue) { // save last field
-        $array[$rowIndex][$columnIndex] = $fieldValue;
-    }
-    return $array;
-}
-
-$csv = csvstring_to_array("1,2,3,4");
-print_r($csv);
-
-$csv = array();
-$lines = file('http://jenna.bendiksens.net/~gruppe2/resultater/20131104_1383603361.xml.csv', FILE_IGNORE_NEW_LINES);
-
-foreach ($lines as $key => $value)
-{
-    $csv[$key] = str_getcsv($value);
-}
-
-echo '<pre>';
-print_r($csv);
-echo '</pre>';
-*/
-
-function parse_csv_file($csvfile) {
-    $csv = Array();
-    $rowcount = 0;
-    if (($handle = fopen($csvfile, "r")) !== FALSE) {
-        $max_line_length = defined('MAX_LINE_LENGTH') ? MAX_LINE_LENGTH : 10000;
-        $header = fgetcsv($handle, $max_line_length, ";");
-        $header_colcount = count($header);
-        while (($row = fgetcsv($handle, $max_line_length, ";")) !== FALSE) {
-            $row_colcount = count($row);
-            if ($row_colcount == $header_colcount) {
-                $entry = array_combine($header, $row);
-                $csv[] = $entry;
-            }
-            else {
-                error_log("csvreader: Invalid number of columns at line " . ($rowcount + 2) . " (row " . ($rowcount + 1) . "). Expected=$header_colcount Got=$row_colcount");
-                return null;
-            }
-            $rowcount++;
-        }
-        //echo "Totally $rowcount rows found\n";
-        fclose($handle);
-    }
-    else {
-        error_log("csvreader: Could not read CSV \"$csvfile\"");
-        return null;
-    }
-    return $csv;
-}
-
-
-/*
-$CSV = array();
-$file = fopen('http://jenna.bendiksens.net/~gruppe2/resultater/20131104_1383603361.xml.csv', 'r');
-while (($line = fgetcsv($file)) !== FALSE) {
-  //$line is an array of the csv elements
-  //startTimeAsLong;startTime;opplos;sim;forbruk;varmetap
-  array_push($CSV, csvstring_to_array($line));
-  
-  
-}
-
-fclose($file);
- */
-$CSV  = parse_csv_file("http://jenna.bendiksens.net/~gruppe2/resultater/20131104_1383603361.xml.csv");
+<?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-echo "<br>--------------------------<br>";
-echo "<br>RAW DATA FROM ARRAY<br>";
-echo "<br>--------------------------<br>";
+if($_GET['x'] == 'submit')
+{
 
+	function parse_csv_file($csvfile) {
+	    $csv = Array();
+	    $rowcount = 0;
+	    if (($handle = fopen($csvfile, "r")) !== FALSE) {
+	        $max_line_length = defined('MAX_LINE_LENGTH') ? MAX_LINE_LENGTH : 10000;
+	        $header = fgetcsv($handle, $max_line_length, ";");
+	        $header_colcount = count($header);
+	        while (($row = fgetcsv($handle, $max_line_length, ";")) !== FALSE) {
+	            $row_colcount = count($row);
+	            if ($row_colcount == $header_colcount) {
+	                $entry = array_combine($header, $row);
+	                $csv[] = $entry;
+	            }
+	            else {
+	                error_log("csvreader: Invalid number of columns at line " . ($rowcount + 2) . " (row " . ($rowcount + 1) . "). Expected=$header_colcount Got=$row_colcount");
+	                return null;
+	            }
+	            $rowcount++;
+	        }
+	        //echo "Totally $rowcount rows found\n";
+	        fclose($handle);
+	    }
+	    else {
+	        error_log("csvreader: Could not read CSV \"$csvfile\"");
+	        return null;
+	    }
+	    return $csv;
+	}
+	if($_POST['dropdown'] != "nothing" && $_POST['dropdown2'] != "nothing" )
+	{
+		$CSV  = parse_csv_file($_POST['dropdown']);
 
-$antall = count($CSV);
+		$antall = count($CSV);
+		
+		echo "
+		<div style='float: left; margin-right: 5px;'>
+			<table border='1'>
+			<tr>
+			<td><strong>startTime</strong></td>
+			<td><strong>forbruk</strong></td>
+			</tr>
+		
+		";
+		
+		for($i=0; $i<$antall;$i++) 
+		{
+			echo "<tr>";
+			echo "<td>".$CSV[$i][startTime]."</td>";
+			echo "<td>".$CSV[$i][forbruk]."</td>";
+			echo "</tr>";
+		}
+		echo "</table>
+		</div>";
+		
+		$CSV2  = parse_csv_file($_POST['dropdown2']);
 
-echo "
-<table border='1'>
-<tr>
-<td><strong>startTime</strong></td>
-<td><strong>forbruk</strong></td>
-</tr>
-
-";
-
-for($i=0; $i<$antall;$i++) {
-	echo "<tr>";
-	echo "<td>".$CSV[$i][startTime]."</td>";
-	echo "<td>".$CSV[$i][forbruk]."</td>";
-	echo "</tr>";
+		$antall2 = count($CSV2);
+		
+		echo "
+		<div>
+			<table border='1'>
+			<tr>
+			<td><strong>startTime</strong></td>
+			<td><strong>forbruk</strong></td>
+			</tr>
+		
+		";
+		
+		for($i=0; $i<$antall2;$i++) 
+		{
+			echo "<tr>";
+			echo "<td>".$CSV2[$i][startTime]."</td>";
+			echo "<td>".$CSV2[$i][forbruk]."</td>";
+			echo "</tr>";
+		}
+		echo "</table>
+		</div>";
+	}
 }
 ?>
-
-
-</table>
