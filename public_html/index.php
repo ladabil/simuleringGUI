c7530c82f48c9b84cb93446f62ae1bdc4edba20f
RL <ladabil@gmail.com>
2013-11-07 20:26:00 +0100

    .

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL);
+
 require_once(dirname(__FILE__) . "/../hidden/config.inc.php");
 require_once($GLOBALS["cfg_hiddendir"] . "/AuthLib.class.inc.php");
 require_once($GLOBALS["cfg_hiddendir"] . "/Site.class.inc.php");
