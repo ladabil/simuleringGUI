a02008d7e4711cd6809539cfdac874deecff15a1
spriti <spriti@mail.com>
2013-11-08 10:05:10 +0100

    .

--- hidden/Site.class.inc.php
+++ hidden/Site.class.inc.php
@@ -2291,8 +2291,8 @@ class Site
 		
 //		print_r($weatherStations);
 
-		$infoMsg = "Oppdatering/innlegging av v&aelig;rstasjoner gikk bra ";
-		$infoMsg .= " (Antall v&aelig;rstasjoner: " . count($weatherStations) . ")";
+		$infoMsg = "Oppdatering/innlegging av værstasjoner gikk bra ";
+		$infoMsg .= " (Antall værstasjoner: " . count($weatherStations) . ")";
 //		$infoMsg = "(Nye: ";
 //		$infoMsg .= (count($weatherStations) - $numUpdated) . " - Oppdaterte: " . $numUpdated . ")";
 		
